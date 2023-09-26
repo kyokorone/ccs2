b05886ef65ed6392896dc5b78e72ffff53b71711
angela sol <145556530+kyokorone@users.noreply.github.com>
2023-09-26 10:23:53 +0800

    Add files via upload

---
+++ henson_numbers.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+/*
+Write a program that will ask for an integer input (1 - 9999). Your program will convert the inputted integer into its corresponding word format in English. See examples below:
+
+Example 1:
+
+Input number: 2481
+
+Output: two thousand four hundred eighty one
+
+Example 2:
+
+Input number: 616
+
+Output: six hundred sixteen
+
+Example 3:
+
+Input number: 30
+
+Output: thirty
+
+- Make sure to trap your input. Any integer input below 1 or above 9999 is an invalid input. In case of an invalid input, the program will display an error message and will exit the program.
+
+-Submit your .C file.
+
+- Find a way to be able to extract each digit according to its place value.
+
+- VERY IMPORTANT: THE SOLUTION DOES NOT USE 9999 IF-STATEMENTS!
+
+- Be careful on the numbers from 10 - 19.
+*/
+
+main()
+{
+	int num, ones, tens, hundreds, thousandths;
+	num
+	
+	printf("Input number: ");
+	scanf("%d,", &num);
+	
+}
