c0d9063a76629d20da555356ff8c3aebd6476e05
angela sol <145556530+kyokorone@users.noreply.github.com>
2023-11-07 10:04:54 +0800

    Add files via upload

---
+++ henson_atm.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	int choice;
+	float bal;
+	
+	
+	
+	// MENU
+	printf("[1] Deposit\n[2] Withdraw\n[3] Balance Inquiry\n[4] Fast Cash\n[5] Exit\n\nENTER CHOICE: ");
+
+}
