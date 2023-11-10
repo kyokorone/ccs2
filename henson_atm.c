56da9d4d54595152168b0b240c4d7a6a9638b8cf
angela sol <145556530+kyokorone@users.noreply.github.com>
2023-11-10 10:08:20 +0800

    henson_atm.c

--- henson_atm.c
+++ henson_atm.c
@@ -3,12 +3,24 @@
 
 void main()
 {
+	
 	int choice;
-	float bal;
-	
-	
-	
+	float balance;
+
 	// MENU
+	balance = 0.00;
 	printf("[1] Deposit\n[2] Withdraw\n[3] Balance Inquiry\n[4] Fast Cash\n[5] Exit\n\nENTER CHOICE: ");
 
+	
+	switch(choice)
+	{
+		case 1: balance = deposit(balance); break;
+		case 2: balance = withdraw(balance); break;
+		// case 3: balance = inquiry(balance); break;
+		case 4: balance = fastcash(balance); break;
+		// case 5: exit;
+	}
+	if (choice != 5){
+	   printf("Your balance is P%d.", balance);
+	   }
 }
